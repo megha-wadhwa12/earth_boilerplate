1d04e8ad9f6375ecbe6ce0b79bf2f3d2480afa88
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2023-08-27 22:45:22 +0530

    try

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ people.onclick=()=>{
 
 
 
-//background
+//Background
 bodyvar.style.background = "url('https://kq-storage.s3.ap-south-1.amazonaws.com/Github/earth/poverty-bg%5B1%5D.png')"
 bodyvar.style.backgroundSize = "100% 100%"
 
